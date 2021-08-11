adf449f1e68719605a9a9603b4784ed67b1fdf0b
Stepan-Nikolaev <me.ctenah@gmail.com>
2021-08-11 03:37:37 +0300

    HomeworkDelegateAndException

--- Models/Client.cs
+++ Models/Client.cs
@@ -12,5 +12,27 @@ namespace BancSystem.Models
         public string Patronymic { get; set; }
         public int Age { get; set; }
         public int PassportID { get; set; }
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (!(obj is Client))
+            {
+                return false;
+            }
+
+            Client verifiedCient = (Client)obj;
+
+            return PassportID == verifiedCient.PassportID;
+        }
+
+        public override int GetHashCode()
+        {
+            return PassportID;
+        }
+
     }
 }
--- Service/Bank.cs
+++ Service/Bank.cs
@@ -1,7 +1,10 @@
-﻿using BancSystem.Models;
+﻿using BancSystem.Currencies;
+using BancSystem.Exceptions;
+using BancSystem.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using static BancSystem.Service.Bank;
 
 namespace BancSystem.Service
 {
@@ -9,19 +12,100 @@ namespace BancSystem.Service
     {
         public List<Client> clients = new List<Client>();
         public List<Employee> employees = new List<Employee>();
+        public Dictionary<Client, List<Accaunt>> dataBaseClients = new Dictionary<Client, List<Accaunt>>();
+        public Func<int, CurrencyType, CurrencyType, double> exchangeFunc;
 
         public void Add<T>(T person) where T : IPerson
         {
-            var lokalClient = person as Client;
-            var lokalEmploy = person as Employee;
+            try
+            {
+                var lokalClient = person as Client;
+                var lokalEmploy = person as Employee;
 
-            if (lokalClient != null)
-            {
-                clients.Add(lokalClient);
+                if (lokalClient != null)
+                {
+                    if (lokalClient.Age < 18)
+                    {
+                        throw new AgeLimitException("Сожалеем, но вы не можете стать клиентом нашего банка т.к. вам меньше 18 лет.");
+                    }
+
+                    clients.Add(lokalClient);
+                }
+                else if (lokalEmploy != null)
+                {
+                    if (lokalEmploy.Age < 18)
+                    {
+                        throw new AgeLimitException("Сожалеем, но вы не можете стать сотрудником нашего банка т.к. вам меньше 18 лет.");
+                    }
+
+                    employees.Add(lokalEmploy);
+                }
             }
-            else if (lokalEmploy != null)
+            catch(AgeLimitException e)
             {
-                employees.Add(lokalEmploy);
+                Console.WriteLine(e);
+            }
+        }
+
+        public void MoneyTransfer(int sum, Accaunt firstAccaunt, Accaunt secondAccaunt, Func<int, CurrencyType, CurrencyType, double> exchangeFunc)
+        {
+            if (exchangeFunc == null)
+            {
+                Console.WriteLine("Делегат пустой");
+            }
+            else
+            {
+                try
+                {
+                    if (sum > firstAccaunt.CountMoney)
+                    {
+                        throw new NotEnoughMoney("Не достаточно денег на счету для данной операции");
+                    }
+
+                    if (firstAccaunt.CurrentCurrency.NameCurrency == secondAccaunt.CurrentCurrency.NameCurrency)
+                    {
+                        firstAccaunt.CountMoney -= sum;
+                        secondAccaunt.CountMoney += sum;
+                    }
+                    else
+                    {
+                        firstAccaunt.CountMoney -= sum;
+                        double exchangedSum = exchangeFunc(sum, firstAccaunt.CurrentCurrency, secondAccaunt.CurrentCurrency);
+                        secondAccaunt.CountMoney += (int)exchangedSum;
+                    }
+                }
+                catch(NotEnoughMoney e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        public void AddClintAccaunt(Client newClient, Accaunt newAccaunt)
+        {
+            if (newClient != null && newAccaunt != null)
+            {
+                try
+                {
+                    if (newClient.Age < 18)
+                    {
+                        throw new AgeLimitException("Вы не можете стать нашим клиентом т.к. вам меньше 18 лет");
+                    }
+
+                    if (dataBaseClients.ContainsKey(newClient))
+                    {
+                        dataBaseClients[newClient].Add(newAccaunt);
+                    }
+                    else
+                    {
+                        List<Accaunt> localListAccaunt = new List<Accaunt>() { newAccaunt };
+                        dataBaseClients.Add(newClient, localListAccaunt);
+                    }
+                }
+                catch (AgeLimitException e)
+                {
+                    Console.WriteLine(e);
+                }
             }
         }
 
@@ -47,7 +131,7 @@ namespace BancSystem.Service
                 return clients[indexFindClient];
             }
             else if (lokalEmploy != null)
-            { 
+            {
                 int indexFindEmployee = employees.IndexOf(lokalEmploy);
 
                 return employees[indexFindEmployee];
